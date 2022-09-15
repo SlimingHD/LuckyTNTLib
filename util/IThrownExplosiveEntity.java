ea4397c63f5b8b2dc16db58b89e81c9708203ea7
SlimingHD <94920232+SlimingHD@users.noreply.github.com>
2022-09-15 18:24:05 +0200

    Is halt nix passiert

--- util/IThrownExplosiveEntity.java
+++
@@ -1,8 +0,0 @@
-package luckytntlib.util;
-
-public interface IThrownExplosiveEntity extends IExplosiveEntity{
-
-	public boolean inGround();
-	
-	public boolean hitEntity();
-}
--- util/IllegalTNTException.java
+++
@@ -1,8 +0,0 @@
-package luckytntlib.util;
-
-@SuppressWarnings("serial")
-public class IllegalTNTException extends RuntimeException{
-	public IllegalTNTException(String message) {
-		super(message);
-	}
-}
